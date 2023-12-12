09ef30af2629b778e68a55de0dcec028af19214a
GitHub Action <action@github.com>
2023-12-12 11:12:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/mirnih/codecolor/MyBundle.kt
+++ src/main/kotlin/com/github/mirnih/codecolor/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mirnih.codecolor
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mirnih/codecolor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mirnih/codecolor/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mirnih.codecolor.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mirnih.codecolor.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
